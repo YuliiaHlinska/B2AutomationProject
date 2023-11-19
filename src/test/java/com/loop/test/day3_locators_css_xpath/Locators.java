a5fd7fb882007ee18d8b32e55bb0e7387737b2a1
YuliiaHlinska <glinchiksony@gmail.com>
2023-11-19 11:59:59 -0500

    days 3 locator practice, get title, get current URL

---
+++ src/test/java/com/loop/test/day3_locators_css_xpath/Locators.java
@@ -0,0 +1,47 @@
+package com.loop.test.day3_locators_css_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+    public static void main(String[] args) {
+        //setup the driver
+        WebDriverManager.chromedriver().setup();
+
+        //create driver object
+        WebDriver driver = new ChromeDriver();
+
+        //navigate to google
+        driver.get("https://www.google.com/");
+
+        driver.manage().window().maximize();
+
+        //locate the elemnt
+        WebElement searchBoxWithID = driver.findElement(By.id("APjFqb"));
+        WebElement searchBoxWithName = driver.findElement(By.name("q"));
+        WebElement aboutLink = driver.findElement(By.linkText("About"));
+
+        //aboutLink.click();
+
+
+        //create a string what you want to search
+        searchBoxWithName.sendKeys("Feyruz is handsome");
+
+        searchBoxWithID.clear();
+
+        String textToSearch = "Loop Academy";
+        searchBoxWithID.sendKeys(textToSearch + Keys.ENTER);
+
+
+
+
+
+
+
+    }
+}
